1daa9b79e80b84f9b111863eaa44aa70bad0f28e
Álvar Legaz Aparicio <116595540+AlvarLegaz@users.noreply.github.com>
2024-09-06 15:01:15 +0200

    Update MLP_for_XOR_problem.py

--- 00_MLP/MLP_for_XOR_problem.py
+++ 00_MLP/MLP_for_XOR_problem.py
@@ -1,27 +1,44 @@
+# Este script entrena una red neuronal para resolver el problema XOR utilizando TensorFlow y Keras.
+# El modelo tiene una capa oculta con 16 neuronas y una capa de salida con 1 neurona.
+# La capa oculta utiliza la función de activación 'relu' y la capa de salida utiliza 'sigmoid'.
+# El modelo se entrena durante 1000 épocas y se evalúa su precisión en los datos de entrada.
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Desactiva los mensajes de información y advertencia
 import tensorflow as tf
+import pandas as pd
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 import numpy as np
 
-# Datos de entrada
-X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-# Salidas esperadas
-y = np.array([[0], [1], [1], [0]])
+# Leer el archivo CSV
+df = pd.read_csv('xor_data.csv')
+
+# Mostrar el contenido del archivo
+print(df)
+
+# Definir X e y
+X = df[['Entrada 1', 'Entrada 2']].values
+y = df['Salida'].values
+
+# Parámetros del modelo
+Num_neuronas = 16
+epocas = 1000  # Iteraciones del entrenamiento
 
 # Definir el modelo
 model = Sequential()
-model.add(Dense(2, input_dim=2, activation='relu'))  # Capa oculta con 2 neuronas
+model.add(Dense(Num_neuronas, input_dim=2, activation='relu'))  # Capa oculta con 100 neuronas
 model.add(Dense(1, activation='sigmoid'))  # Capa de salida
 
 # Compilar el modelo
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Entrenar el modelo
-model.fit(X, y, epochs=1000, verbose=0)
+model.fit(X, y, epochs=epocas, verbose=0)
 
 # Evaluar el modelo
 loss, accuracy = model.evaluate(X, y)
-print(f'Precisión: {accuracy*100}%')
+print(f'Precisión: {accuracy*100:.2f}%')
 
 # Hacer predicciones
 predictions = model.predict(X)
